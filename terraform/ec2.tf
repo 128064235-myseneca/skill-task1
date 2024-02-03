2a8748501904f01d33554100d63c9161cc45cd5b
Shubham Joshi <shubham.joshi@deerwalk.edu.np>
2024-02-03 17:19:35 -0500

    added ECR repos

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,18 @@
 data "aws_ami" "aws_image" {
   owners      = ["amazon"]
   most_recent = true
-  
+
 }
 
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.aws_image.id
   instance_type = "t2.micro"
   subnet_id     = aws_default_subnet.default_az1.id
-  key_name = aws_key_pair.mykey.key_name
+  key_name      = aws_key_pair.mykey.key_name
 }
 
 
 resource "aws_key_pair" "mykey" {
-  key_name = "assignment-1"
+  key_name   = "assignment-1"
   public_key = file("assignment1.pub")
 }

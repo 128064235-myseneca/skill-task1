507a9b1b5feed7494e6b3db76aad5e9b498aab80
Shubham Joshi <shubham.joshi@deerwalk.edu.np>
2024-02-04 17:39:16 -0500

    changes in the ports

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,11 +5,12 @@ data "aws_ami" "aws_image" {
 }
 
 resource "aws_instance" "web_server" {
-  ami             = data.aws_ami.aws_image.id
-  instance_type   = "t2.micro"
-  subnet_id       = aws_default_subnet.default_az1.id
-  key_name        = aws_key_pair.mykey.key_name
-  security_groups = [aws_security_group.my_ec2-sg.id]
+  ami                  = data.aws_ami.aws_image.id
+  instance_type        = "t2.micro"
+  subnet_id            = aws_default_subnet.default_az1.id
+  key_name             = aws_key_pair.mykey.key_name
+  security_groups      = [aws_security_group.my_ec2-sg.id]
+  iam_instance_profile = "LabInstanceProfile"
 }
 
 
--- terraform/tg.tf
+++ terraform/tg.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "blue" {
   name     = "tf-blue-lb-tg"
-  port     = 80
+  port     = 8081
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default.id
 }
@@ -8,12 +8,12 @@ resource "aws_lb_target_group" "blue" {
 resource "aws_lb_target_group_attachment" "blue" {
   target_group_arn = aws_lb_target_group.blue.arn
   target_id        = aws_instance.web_server.id
-  port             = 80
+  port             = 8081
 }
 
 resource "aws_lb_target_group" "pink" {
   name     = "tf-pink-lb-tg"
-  port     = 81
+  port     = 8082
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default.id
 }
@@ -21,12 +21,12 @@ resource "aws_lb_target_group" "pink" {
 resource "aws_lb_target_group_attachment" "pink" {
   target_group_arn = aws_lb_target_group.pink.arn
   target_id        = aws_instance.web_server.id
-  port             = 81
+  port             = 8082
 }
 
 resource "aws_lb_target_group" "lime" {
   name     = "tf-lime-lb-tg"
-  port     = 82
+  port     = 8083
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default.id
 }
@@ -34,5 +34,5 @@ resource "aws_lb_target_group" "lime" {
 resource "aws_lb_target_group_attachment" "lime" {
   target_group_arn = aws_lb_target_group.lime.arn
   target_id        = aws_instance.web_server.id
-  port             = 82
+  port             = 8083
 }

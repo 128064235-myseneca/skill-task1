ed23fa133f8325372eb81ba05c4b66d703a6e9cc
joshi-shubham <shubjoshi2056@gmail.com>
2024-02-04 19:16:20 -0500

    changes:

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,12 @@
-data "aws_ami" "aws_image" {
-  owners      = ["amazon"]
-  most_recent = true
+# data "aws_ami" "aws_image" {
+#   owners      = ["amazon"]
+#   most_recent = true
 
-}
+
+# }
 
 resource "aws_instance" "web_server" {
-  ami                  = data.aws_ami.aws_image.id
+  ami                  = "ami-0aae4ed2ead014e0c"
   instance_type        = "t2.micro"
   subnet_id            = aws_default_subnet.default_az1.id
   key_name             = aws_key_pair.mykey.key_name
